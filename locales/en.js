5c50dd38f16ab0ae548caef4c225b9e89f089cf9
andi9010 <andres_solarteo@hotmail.com>
2019-01-19 15:13:23 -0500

    formato en archivo en

--- locales/en.js
+++ locales/en.js
@@ -1,22 +1,15 @@
 export default {
-  'home': 'Home'
-  , 'changeTheme': 'Change Theme'
+  'home': 'Home',
+  'changeTheme': 'Change Theme',
   
-  , 'admin': {
-    'module-name': 'Administration'
-    , 'doc-types': {
-      'module-name': 'Document Types'
-    }
-    , 'annotation-types': {
-      'module-name': 'Annotation Types'
-    }
-    , 'entity-types': {
-      'module-name': 'Entity Types'
-    }
-    , 'entities': {
-      'module-name': 'Entities'
-    }
+  'admin': {
+    'module-name': 'Administration',
+    'doc-types': {'module-name': 'Document Types'},
+    'annotation-types': {'module-name': 'Annotation Types'},
+    'entity-types': {'module-name': 'Entity Types'},
+    'entities': {'module-name': 'Entities',}
   },
+  
   'persons': {
     'module-name': 'Persons',
     'users' : {'module-name': 'Users'},
