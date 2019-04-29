79ee2f8b1f84690282a6463da3ccedfa2247293c
andi9010 <andres_solarteo@hotmail.com>
2019-04-29 18:24:13 -0500

    no autorizado

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,6 +2,14 @@ export default function (ctx) {
   ctx.$axios.onRequest((config) => {
     config.headers.common['Authorization'] = getToken(ctx)
   })
+
+  ctx.$axios.onError(error => {
+    const code = parseInt(error.response && error.response.status)
+    if (code === 401) {      
+      ctx.redirect('/unauthorized')
+    }
+  })
+
 }
 
 function getToken(ctx) {
